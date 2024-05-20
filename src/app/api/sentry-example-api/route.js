e8d22056925326cb1a5e80018cfddb93963eb428
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2024-05-20 14:36:21 +0200

    Add Sentry example API route and page

--- src/app/api/sentry-example-api/route.js
+++ src/app/api/sentry-example-api/route.js
@@ -1,6 +1,6 @@
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 export async function GET(request) {
-  Sentry.captureMessage("Sentry Example API Route Hit");
-  return new Response("Sentry example API route", { status: 200 });
+  Sentry.captureMessage('Sentry Example API Route Hit');
+  return new Response('Sentry example API route', { status: 200 });
 }
